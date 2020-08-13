c614a8eb6a0f0ada977c7bf4d589064454f84477
mmiroshnikov <mikoza+mmiroshnikov@gmail.com>
2020-08-13 16:22:03 -0700

    fix

---
+++ build/precache-manifest.bbfdebb4091a66e8ff0f6cdc0e6b7d6c.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d6e56652bb8676516f74a5d5e9575b9",
+    "url": "/static/media/quest--blue.0d6e5665.svg"
+  },
+  {
+    "revision": "b6685af53c16198a8914",
+    "url": "/static/css/main.00c27e28.chunk.css"
+  },
+  {
+    "revision": "adbd790e12bb0b9c7e525a67dc6d4222",
+    "url": "/static/media/close.adbd790e.svg"
+  },
+  {
+    "revision": "06d238b85d1efd2f3d54",
+    "url": "/static/js/1.06d238b8.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "be4100654759facf39893e8eede84b63",
+    "url": "/static/media/separate-list.be410065.svg"
+  },
+  {
+    "revision": "648c667a65b90f5c8921a1f78956f8d5",
+    "url": "/static/media/checkmark-in-circle-transparent.648c667a.svg"
+  },
+  {
+    "revision": "b529a0c2b87ddb0087525c829c582e87",
+    "url": "/static/media/alert--red-transparent.b529a0c2.svg"
+  },
+  {
+    "revision": "a3d75bc3c2ad42dcfe4472177f9e148f",
+    "url": "/static/media/alert--transparent.a3d75bc3.svg"
+  },
+  {
+    "revision": "c5c1c9cf5d591d6c025d4102a0b33503",
+    "url": "/static/media/alert--orange-transparent.c5c1c9cf.svg"
+  },
+  {
+    "revision": "b6685af53c16198a8914",
+    "url": "/static/js/main.b6685af5.chunk.js"
+  },
+  {
+    "revision": "0228ccc75c5d6cb507728b6fa3171b5a",
+    "url": "/static/media/quest.0228ccc7.svg"
+  },
+  {
+    "revision": "2368a18dc9373872e843430eb78146f9",
+    "url": "/static/media/checked--grey.2368a18d.svg"
+  },
+  {
+    "revision": "5400494c34c50565931664b1dba53c3e",
+    "url": "/static/media/checked--red.5400494c.svg"
+  },
+  {
+    "revision": "1e7c71cee64e9d0d642c0b093f4f39e8",
+    "url": "/static/media/checked.1e7c71ce.svg"
+  },
+  {
+    "revision": "e822c49655db4e28b7e5e9ba13c90ac7",
+    "url": "/static/media/xsolla-pattern.e822c496.svg"
+  },
+  {
+    "revision": "d926abab7549e4c755871288d5c75984",
+    "url": "/static/media/loader--green.d926abab.svg"
+  },
+  {
+    "revision": "57d13acd0e98a391cbb0c4f8416913b4",
+    "url": "/static/media/loader--border.57d13acd.svg"
+  },
+  {
+    "revision": "06d238b85d1efd2f3d54",
+    "url": "/static/css/1.cd88f8e1.chunk.css"
+  },
+  {
+    "revision": "169a15adaa0b65c7a4db236975e61b21",
+    "url": "/index.html"
+  }
+];
